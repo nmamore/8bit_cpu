9ccba70ade2515cbb9d86ef38e6e093371895750
Nick A <namore7@gmail.com>
2022-05-01 21:17:15 -0400

    5/1/22 Commit
    Adds 8-bit register. Updates testbench heavily. Bugs to iron out still

--- RTL/cpu_8bit_top.sv
+++ RTL/cpu_8bit_top.sv
@@ -5,26 +5,32 @@
 * @date Created 4/29/2022
 */
 
-
 `timescale 1ns / 100ps
 
 module cpu_8bit_top
 (
 	input logic i_clk,
-	input logic i_rstn
+	input logic i_rstn,
+	
+	input logic i_a_wrtn,
+	input logic i_a_rdn,
+	input logic i_b_wrtn,
+	input logic i_b_rdn,
+	
+	inout logic [7:0] io_data_bus,
+	
+	input logic [3:0] i_alu_opcode,
+	input logic i_cin,
+	
+	input logic i_alu_sel,
+	input logic i_alu_flag_sel
 );
 
+//Data from registers
 logic [7:0] a_data;
 logic [7:0] b_data;
-logic [7:0] alu_out;
 
-logic [3:0] alu_op;
-
-logic alu_sel;
-logic flag_sel;
-
-logic cin;
-
+//ALU flags
 logic zr;
 logic ng;
 logic pa;
@@ -35,23 +41,49 @@ alu ALU_module(
 	.i_a_data(a_data),
 	.i_b_data(b_data),
 	
-	.i_alu_sel(alu_sel),
+	.i_alu_sel(i_alu_sel),
 	
 	.i_clk(i_clk),
 	.i_rstn(i_rstn),
-	.i_flag_sel(flag_sel),
+	.i_flag_sel(i_alu_flag_sel),
 	
-	.i_cin(cin),
+	.i_cin(i_cin),
 	
-	.i_alu_op(alu_op),
+	.i_alu_op(i_alu_opcode),
 	
-	.o_out_data(alu_out),
+	.o_out_data(io_data_bus),
 	
 	.o_zr(zr),
 	.o_ng(ng),
 	.o_pa(pa),
 	.o_co(co),
 	.o_of(of)
+);
+
+register_8bit a_register
+(
+	.io_bus_data(io_data_bus),
+	
+	.i_clk(i_clk),
+	.i_rstn(i_rstn),
+	
+	.i_rdn(i_a_rdn),
+	.i_wrtn(i_a_wrtn),
+	
+	.o_out_data(a_data)
+);
+
+register_8bit b_register
+(
+	.io_bus_data(io_data_bus),
+	
+	.i_clk(i_clk),
+	.i_rstn(i_rstn),
+	
+	.i_rdn(i_b_rdn),
+	.i_wrtn(i_b_wrtn),
+	
+	.o_out_data(b_data)
 );
 
 endmodule
--- TESTBENCH/tb_8bit_cpu_top.sv
+++ TESTBENCH/tb_8bit_cpu_top.sv
@@ -1,188 +1,207 @@
 /*
 * @file tb_8bit_cpu.sv
-* @brief top-level file for the nand2hack testbench
+* @brief top-level file for the 8-bit CPU testbench
 * @author Nicholas Amore namore7@gmail.com
 * @date Created 4/29/2022
 */
 
 `timescale 1ns/1ps
 
-module tb_cpu_8bit ();
+module tb_cpu_8bit();
+	
+wire pld_mclk;
+wire pld_rstn;
+wire [7:0] data_bus;
 
-// ref clock = 24.00MHz => 41.7ns, For half of clock, PLD_CLK_SPEED = 20.9ns
-localparam PLD_CLK_SPEED = 20.9;
-	
-logic pld_mclk;
-logic pld_rstn;
-	
+wire a_wrtn;
+wire a_rdn;
+wire b_wrtn;
+wire b_rdn;
+
+wire [3:0] alu_opcode;
+wire cin;
+
+wire alu_sel;
+wire alu_flag_sel;
+
 cpu_8bit_top uut (
 	.i_clk(pld_mclk),
-	.i_rstn(pld_rstn)
-);
+	.i_rstn(pld_rstn),
+	.io_data_bus(data_bus),
 	
+	.i_a_wrtn(a_wrtn),
+	.i_a_rdn(a_rdn),
+	.i_b_wrtn(b_wrtn),
+	.i_b_rdn(b_rdn),
+	
+	.i_alu_opcode(alu_opcode),
+	.i_cin(cin),
+	
+	.i_alu_sel(alu_sel),
+	.i_alu_flag_sel(alu_flag_sel)
+);
+
+cpu_drive drive (	
+	.o_pld_clk(pld_mclk),
+	.o_pld_rstn(pld_rstn),
+	
+	.o_a_wrtn(a_wrtn),
+	.o_a_rdn(a_rdn),
+	.o_b_wrtn(b_wrtn),
+	.o_b_rdn(b_rdn),
+	
+	.o_alu_opcode(alu_opcode),
+	.o_cin(cin),
+	
+	.o_alu_sel(alu_sel),
+	.o_alu_flag_sel(alu_flag_sel),
+	
+	.io_databus(data_bus)
+);
 testbench test();
-//reference clock for CPU
-initial begin
-	$display("PLD clk started");
-	pld_mclk = 1'b0;
-	#15;
-	forever
-	begin
-		#PLD_CLK_SPEED pld_mclk = 1'b1; //wait half period then toggle clock
-		#PLD_CLK_SPEED pld_mclk = 1'b0;
-	end
-end
 	
 //run tests
 initial begin
 	#2000;
-	pld_reset;
-	test_ALUop();
-	test_ALUflag();
+	test_registerWrite();
+	test_registerRead();
 	$stop;
 end
-
-/**
-* @brief Reset PLD
-* @detailed This test resets the PLD
-*/
-	
-task pld_reset();
-	pld_rstn = 1'b0;
-	#200;
-	pld_rstn = 1'b1;
-	#500;	
-endtask
 	
 /**
 * @brief automated test for the ALU
 * @detailed This test verifies that the ALU properly performs all 16 operations
 */
 	
-task test_ALUop();
+/*task test_ALUop();
 	
 	//Tests pass through of A register
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'h0);
 	alu_desel();
-	test.checkEquality(8'hA5, uut.alu_out);
+	test.checkEquality(8'hA5, data_bus);
 	#100;
 	
 	//Tests pass through of B register
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'h1);
 	alu_desel();
-	test.checkEquality(8'h5A, uut.alu_out);
+	test.checkEquality(8'h5A, data_bus);
 	#100;
 	
 	//Test AND of A and B register
 	writeALU(8'hA5, 8'h39, 1'b0, 4'h2);
 	alu_desel();
-	test.checkEquality(8'h21, uut.alu_out);
+	test.checkEquality(8'h21, data_bus);
 	#100;
 	
 	//Test OR of A and B register
 	writeALU(8'hA5, 8'h39, 1'b0, 4'h3);
 	alu_desel();
-	test.checkEquality(8'hBD, uut.alu_out);
+	test.checkEquality(8'hBD, data_bus);
 	#100;
 	
 	//Test XOR of A and B register
 	writeALU(8'hA5, 8'h39, 1'b0, 4'h4);
 	alu_desel();
-	test.checkEquality(8'h9C, uut.alu_out);
+	test.checkEquality(8'h9C, data_bus);
 	#100;
 	
 	//Tests addition without carry in
 	writeALU(8'h03, 8'h04, 1'b0, 4'h5);
 	alu_desel();
-	test.checkEquality(8'h07, uut.alu_out);
+	test.checkEquality(8'h07, data_bus);
 	#100;
 	
 	//Tests addition with carry in
 	writeALU(8'h03, 8'h04, 1'b1, 4'h5);
 	alu_desel();
-	test.checkEquality(8'h08, uut.alu_out);
+	test.checkEquality(8'h08, data_bus);
 	#100;
 	
 	//Tests Subtraction without carry in
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'h6);
 	alu_desel();
-	test.checkEquality(8'h4B, uut.alu_out);
+	test.checkEquality(8'h4B, data_bus);
 	#100;
 	
 	//Tests Subtraction with carry in
 	writeALU(8'hA5, 8'h5A, 1'b1, 4'h6);
 	alu_desel();
-	test.checkEquality( 8'h4C, uut.alu_out);
+	test.checkEquality( 8'h4C, data_bus);
 	#100;
 	
 	//Tests zeroing the output
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'h7);
 	alu_desel();
-	test.checkEquality(8'h00, uut.alu_out);
+	test.checkEquality(8'h00, data_bus);
 	#100;
 	
 	//Tests 1's compliment of A input
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'h8);
 	alu_desel();
-	test.checkEquality(8'h5A, uut.alu_out);
+	test.checkEquality(8'h5A, data_bus);
 	#100;
 	
 	//Tests 2's compliment of A input
 	writeALU(8'hA5, 8'h5A, 1'b1, 4'h8);
 	alu_desel();
-	test.checkEquality(8'h5B, uut.alu_out);
+	test.checkEquality(8'h5B, data_bus);
 	#100;
 	
 	//Tests 1's compliment of B input
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'h9);
 	alu_desel();
-	test.checkEquality(8'hA5, uut.alu_out);
+	test.checkEquality(8'hA5, data_bus);
 	#100;
 	
 	//Tests 2's compliment of B input
 	writeALU(8'hA5, 8'h5A, 1'b1, 4'h9);
 	alu_desel();
-	test.checkEquality(8'hA6, uut.alu_out);
+	test.checkEquality(8'hA6, data_bus);
 	#100;
 	
 	//Tests increment of A input
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'hA);
 	alu_desel();
-	test.checkEquality(8'hA6, uut.alu_out);
+	test.checkEquality(8'hA6, data_bus);
 	#100;
 	
 	//Tests decrement of A input
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'hB);
 	alu_desel();
-	test.checkEquality(8'hA4, uut.alu_out);
+	test.checkEquality(8'hA4, data_bus);
 	#100;
 	
 	//Tests logical shift left
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'hC);
 	alu_desel();
-	test.checkEquality(8'h4A, uut.alu_out);
+	test.checkEquality(8'h4A, data_bus);
 	#100;
 	
 	//Tests logical shift right
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'hD);
 	alu_desel();
-	test.checkEquality(8'h52, uut.alu_out);
+	test.checkEquality(8'h52, data_bus);
 	#100;
 	
 	//Tests arithmetic shift left
 	writeALU(8'hA5, 8'h5A, 1'b0, 4'hE);
 	alu_desel();
-	test.checkEquality(8'h4B, uut.alu_out);
+	test.checkEquality(8'h4B, data_bus);
 	#100;
 	
 	//Tests bit reveral
 	writeALU(8'hCC, 8'h5A, 1'b0, 4'hF);
 	alu_desel();
-	test.checkEquality(8'h33, uut.alu_out);
+	test.checkEquality(8'h33, data_bus);
 	#100;
-endtask
+endtask/*
 
-task test_ALUflag();
+/**
+* @brief automated test for the ALU flags
+* @detailed Performs various ALU operations that will force flags to set
+*/
+
+/*task test_ALUflag();
 	
 	//Tests zero flag
 	writeALU(8'h00, 8'h5A, 1'b0, 4'h0);
@@ -243,29 +262,25 @@ task test_ALUflag();
 	alu_desel();
 	test.checkEquality(1'b0, uut.of);
 	#100;
-endtask
+endtask*/
 
-task writeALU(
-	input [7:0] a_data,
-	input [7:0] b_data,
-	input cin,
-	input [3:0] alu_opcode
-);
-
-	uut.a_data = a_data;
-	uut.b_data = b_data;
-	uut.alu_op = alu_opcode;
-	uut.cin = cin;
+task test_registerWrite();
 	
-	@(negedge pld_mclk);
-	uut.alu_sel = 1'b1;
-	uut.flag_sel = 1'b1;	
+	drive.registerWrite(1'b0, 8'hA5);
+	drive.registerWrite(1'b1, 8'h5A);
+
 endtask
 
-task alu_desel();
-	@(negedge pld_mclk);
-	uut.alu_sel = 1'b0;
-	uut.flag_sel = 1'b0;	
+task test_registerRead();
+	
+	reg [7:0] data_array [0:1];
+	
+	drive.registerRead(1'b0, data_array[0]);
+	drive.registerRead(1'b1, data_array[1]);
+	
+	test.checkEquality(8'hA5, data_array[0]);
+	test.checkEquality(8'h5A, data_array[1]);
+
 endtask
 
 endmodule
